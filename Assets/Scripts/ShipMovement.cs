b276bd863e3451ff748e7bdadbaaa3e641c87a65
RickyMorel <ricky@attackwagon.com>
2022-09-23 09:51:29 -0400

    Fixed Input

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInputHandler))]
 public class ShipMovement : MonoBehaviour
 {
+    #region Editor Fields
+
+    [SerializeField] private Transform _shipBooster;
+
+    #endregion
+
     #region Private Variables
 
     private float _posX, _posY, _angle = 0;
@@ -15,28 +20,29 @@ public class ShipMovement : MonoBehaviour
 
     public float AngularSpeed, RotationRadius;
     public Transform RotationCenter;
+
     #endregion
     
     #region Unity Loops
     
     private void Start() 
     {
-        _playerInput = GetComponent<PlayerInputHandler>();
+        _playerInput = FindObjectOfType<PlayerInputHandler>();
     }
 
     private void Update() 
     {
         Rotate();
     }
+    #endregion
 
     private void Rotate()
     {
-        _posX = RotationCenter.position.x+_playerInput.MoveDirection.x*RotationRadius;
+        _posX = RotationCenter.position.x + _playerInput.MoveDirection.x * RotationRadius;
         Debug.Log("posX =" + _posX);
-        _posY = RotationCenter.position.y+_playerInput.MoveDirection.y*RotationRadius;
+        _posY = RotationCenter.position.y + _playerInput.MoveDirection.y * RotationRadius;
         Debug.Log("posY =" + _posY);
-        transform.position = new Vector3(_posX, _posY, 0);
+        _shipBooster.position = new Vector3(_posX, _posY, 0);
         gameObject.transform.rotation = Quaternion.Euler(0, 0, _angle);
     }
-    #endregion
 }
