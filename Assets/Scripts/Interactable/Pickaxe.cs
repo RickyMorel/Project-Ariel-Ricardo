de5c6000312cf7d8b3c0a30fe4336129a1377f69
RickyMorel <ricky@attackwagon.com>
2022-11-19 10:18:28 -0300

    Basic Pickaxe Movement

--- Assets/Scripts/Interactable/Pickaxe.cs
+++ Assets/Scripts/Interactable/Pickaxe.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Pickaxe : RotationalInteractable
 {
+    #region Editor Fields
+
+    [SerializeField] private float _acceleration = 1.0f;
+    [SerializeField] private float _topAcleration = 21f;
+    [SerializeField] private float _pickaxeDrag = 0.1f;
+    [SerializeField] private float _noramlTopSpeed = 200f;
+    [SerializeField] private float _boostTopSpeed = 200f;
+
+    #endregion
+
     #region Private Variables
 
+    private float _initialRotationSpeed;
+    private float _topSpeed;
     private bool _isBoosting = false;
 
     #endregion
 
     #region Unity Loops
+
+    public override void Start()
+    {
+        base.Start();
+
+        _initialRotationSpeed = RotationSpeed;
+    }
 
     public override void Update()
     {
@@ -23,6 +42,13 @@ public class Pickaxe : RotationalInteractable
         SetIsBoosting(true);
     }
 
+    private void FixedUpdate()
+    {
+        BoostPickaxe();
+        ApplyDrag();
+
+    }   
+
     #endregion
 
     private void SetIsBoosting(bool isBoosting)
@@ -31,10 +57,31 @@ public class Pickaxe : RotationalInteractable
         if (_isBoosting == isBoosting) { return; }
 
         _isBoosting = isBoosting;
+    }
 
-        //OnBoostUpdated?.Invoke(isBoosting);
+    private void ApplyDrag()
+    {
+        if(CurrentAngle == 0) { return; }
 
-        //if (isBoosting)
-        //    BoostImpulse();
+        if((_isBoosting || _currentPlayer.MoveDirection.magnitude > 0) && CurrentAngle < _topSpeed) { return; }
+
+        CurrentAngle -= CurrentAngle * _pickaxeDrag * Time.deltaTime;
+    }
+
+    private void BoostPickaxe()
+    {
+        _topSpeed = _isBoosting ? _boostTopSpeed : _noramlTopSpeed;
+    }
+
+    public override void Rotate()
+    {
+        float acceleration = RotationSpeed * _currentPlayer.MoveDirection.x * Time.deltaTime;
+
+        //Start slowing down if surpassed top speed
+        if (CurrentAngle > _topSpeed) { acceleration = 0; }
+
+        CurrentAngle = Mathf.Clamp(CurrentAngle + acceleration, -_initialRotationSpeed, _boostTopSpeed);
+
+        RotatorTransform.RotateAround(PivotTransform.position, Vector3.forward, -CurrentAngle);
     }
 }
