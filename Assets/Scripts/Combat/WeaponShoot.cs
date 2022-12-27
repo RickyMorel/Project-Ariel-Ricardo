0e0a8af806a15413d88be830dc23dd8c0c646361
Arielelnoob115Z <arguelloariel115@gmail.com>
2022-12-27 08:53:23 -0300

    Removed some stuff

--- Assets/Scripts/Combat/WeaponShoot.cs
+++ Assets/Scripts/Combat/WeaponShoot.cs
@@ -51,8 +51,6 @@ public class WeaponShoot : MonoBehaviour
 
     public void ProjectileShootAngle()
     {
-        //If you are going to add Quaternions, always remember to use Euler, or else you will just have a road of pain, suffering and confusion.
-        //Also when trying to add Quaternions, you need to multiply instead of add for some reason
         if (_amountOfProjectiles%2 == 1)
         {
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation);
@@ -60,8 +58,6 @@ public class WeaponShoot : MonoBehaviour
         }
         else
         {
-            //You need to instantiate 2 projectiles with the angle halved, since the separation angle only accounts for shots with the same separation angle
-            //This means that the first two shots are going to have separation angle that is double, since the separation starts at the center of the barrel
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(_shootSpreadSeparationAngle / 2, 0, 0));
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(-_shootSpreadSeparationAngle / 2, 0, 0));
             ProjectileInstantiation(1);
@@ -70,7 +66,6 @@ public class WeaponShoot : MonoBehaviour
 
     private void ProjectileInstantiation(int isOddOrEven)
     {
-        //If the number of projectiles is odd the variable needs to be 0, otherwise 1
         for (float i = (1+isOddOrEven); i < _amountOfProjectiles; i += 2)
         {
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(_shootSpreadSeparationAngle * (i / 2 + 0.5f), 0, 0));
