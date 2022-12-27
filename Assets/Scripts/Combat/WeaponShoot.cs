1378568b16d65a7e18861bc99bd6b7d08b853d6b
Arielelnoob115Z <arguelloariel115@gmail.com>
2022-12-27 09:02:42 -0300

    Added comments

--- Assets/Scripts/Combat/WeaponShoot.cs
+++ Assets/Scripts/Combat/WeaponShoot.cs
@@ -51,6 +51,7 @@ public class WeaponShoot : MonoBehaviour
 
     public void ProjectileShootAngle()
     {
+        //We first check if the amount of projectiles is odd or even, if it is odd, we'll instantiate a projectile in the middle of the cone
         if (_amountOfProjectiles%2 == 1)
         {
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation);
@@ -58,6 +59,9 @@ public class WeaponShoot : MonoBehaviour
         }
         else
         {
+            //If it is even you need to instantiate 2 projectiles with the angle halved, since the separation angle only accounts for shots with the same separation angle
+            //This means that the first two shots are going to have separation angle that is double, since the separation starts at the center of the barrel
+            //This is not an issue with the odd, since the projectile of the middle makes it so that the angle of separation is correct
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(_shootSpreadSeparationAngle / 2, 0, 0));
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(-_shootSpreadSeparationAngle / 2, 0, 0));
             ProjectileInstantiation(1);
@@ -66,6 +70,8 @@ public class WeaponShoot : MonoBehaviour
 
     private void ProjectileInstantiation(int isOddOrEven)
     {
+        //If the number of projectiles is odd the variable needs to be 0, otherwise 1
+        //It needs to be 1 or else, it will spawn unnecesary projectiles with an incorrect angle
         for (float i = (1+isOddOrEven); i < _amountOfProjectiles; i += 2)
         {
             Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransform.position, _weapon.TurretHead.rotation * Quaternion.Euler(_shootSpreadSeparationAngle * (i / 2 + 0.5f), 0, 0));
